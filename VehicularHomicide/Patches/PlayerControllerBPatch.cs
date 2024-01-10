06b3ffac3746d088af43e4b2af77098739e13fcb
Chase A <99326567+ActuallyChaseyy@users.noreply.github.com>
2024-01-10 22:09:21 +1100

    Add project files.

---
+++ VehicularHomicide/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,32 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicularHomicide.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float ___sprintMeter, ref float ___sprintMultiplier, ref bool ___isSprinting)
+        {
+            ___sprintMeter = 1f;
+
+            if (___isSprinting)
+            {
+                ___sprintMultiplier = 5f;
+            }
+            else
+            {
+                ___sprintMultiplier = 10f;
+            }
+        }
+
+    }
+}
---
+++ VehicularHomicide/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicularHomicide.Patches;
+
+namespace VehicularHomicide
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ModBaseReal : BaseUnityPlugin
+    {
+        private const string modGUID = "net.chaseyy.vehicularhomicide";
+        private const string modName = "Vehicular Homicide";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ModBaseReal Instance;
+
+        internal ManualLogSource mls;
+        void Awake()
+        {
+            if (Instance == null) { Instance = this; }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Vehicular Homicide Loaded");
+
+            harmony.PatchAll(typeof(ModBaseReal));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+
+        }
+
+    }
+}
